d6635fbf0addc1428d05640c9bcfb8cf1b7d8746
eunkang <ek646@cornell.edu>
2023-02-25 18:27:16 -0500

    api test

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 
 let active = false;
+const btn_caption = document.getElementById("active-livecaption")
 
 async function getTabId() {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-    console.log(tab)
     return tab.id;
 }
-
-
 
 async function setCaption(e) {
     e.preventDefault();
     active = !active;
     if (active) {
-        
             chrome.scripting.executeScript({
             target: { tabId: await getTabId() },
             files: ["scripts/livecaption.js"]
@@ -23,7 +20,7 @@ async function setCaption(e) {
                 files:["styles.css"]
             })
     
-    }
+    } 
 }
 
-document.getElementById("active-livecaption").addEventListener("click", setCaption)
+btn_caption.addEventListener("click", setCaption)
