fdfa453d6255b4b3362626580416a78383056eb4
eunkang <ek646@cornell.edu>
2023-03-01 00:03:12 -0500

    add bug

--- src/livecaption.js
+++ src/livecaption.js
@@ -5,23 +5,20 @@ dotenv.config();
 
 //fetch wether
 const weatherInfo = process.env.WEATHER;
-console.log(weatherInfo)
 const video = document.getElementById('player');
 
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${weatherInfo}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data.weather[0].description)
+        const weatherData = data.weather[0].description
+        const testScript = document.createTextNode(weatherData);
+        const injectScript = document.createElement('div');
+        injectScript.classList.add('caption-container');
+        injectScript.appendChild(testScript);
+        video.after(injectScript);
+
+    }).catch(err => console.log(err));   
+
+
     
-    })
-    .catch(err => console.log(err));   
 
-    
-
-const testScript = document.createTextNode(`Hi How are you?`);
-const injectScript = document.createElement('div');
-injectScript.classList.add('caption-container');
-injectScript.appendChild(testScript);
-
-
-video.after(injectScript);
